634327ec096f343c40d3737ee5081c9559aec8fb
mithsin <hsinbluemoon@gmail.com>
2018-06-06 19:54:09 -0400

    bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,79 +1,74 @@
+$(document).ready(function(){
+    var ranNum = Math.floor(Math.random()*120-19)+19;
+    var red = Math.floor(Math.random()*12-1)+1;
+    var blue = Math.floor(Math.random()*12-1)+1;
+    var green = Math.floor(Math.random()*12-1)+1;
+    var yellow = Math.floor(Math.random()*12-1)+1;
+    var win = 0;
+    var loss = 0;
+    var point = 0;
 
-    $(document).ready(function(){
-        var ranNum = Math.floor(Math.random()*120-19)+19;
-        var red = Math.floor(Math.random()*12-1)+1;
-        var blue = Math.floor(Math.random()*12-1)+1;
-        var green = Math.floor(Math.random()*12-1)+1;
-        var yellow = Math.floor(Math.random()*12-1)+1;
-        var win = 0;
-        var loss = 0;
-        var point = 0;
-        
-        // print number to start the game
+    // print number to start the game
+    $("#ranNum").html(ranNum);
+    $("#wins").html(win);
+    $("#losses").html(loss);
+    $("#point").html(point);
+
+
+    // reset after win or loss
+    function reset(){
+        point = 0;
+        ranNum = Math.floor(Math.random()*120-19)+19;
+        red = Math.floor(Math.random()*12-1)+1;
+        blue = Math.floor(Math.random()*12-1)+1;
+        green = Math.floor(Math.random()*12-1)+1;
+        yellow = Math.floor(Math.random()*12-1)+1;
+
         $("#ranNum").html(ranNum);
-        $("#wins").html(win);
-        $("#losses").html(loss);
         $("#point").html(point);
 
-        
-        // reset after win or loss
-        function reset(){
-            point = 0;
-            var ranNum = Math.floor(Math.random()*120-19)+19;
-            var red = Math.floor(Math.random()*12-1)+1;
-            var blue = Math.floor(Math.random()*12-1)+1;
-            var green = Math.floor(Math.random()*12-1)+1;
-            var yellow = Math.floor(Math.random()*12-1)+1;
+      };
 
-            $("#ranNum").html(ranNum);
-            $("#point").html(point);
-                            //delete when complete
-                            $("#sred").html(red);
-                            $("#sblue").html(blue);
-                            $("#sgreen").html(green);
-                            $("#syellow").html(yellow);
-        	};
-        
-        function gift(){
-       		if (point == ranNum) {
-                winner();
-                } else if (point > ranNum){
-                losser();
-                }
-       	 };
-        
-        // onClick for crystal
-        $("#red").on("click", function(){
-            point = point + red;
-            $("#point").html(point);
-            gift();
-         	});
-        $("#blue").on("click", function(){
-            point = point + blue;
-            $("#point").html(point);
-            gift();
-        	});
-            $("#green").on("click", function(){
-            point = point + green;
-            $("#point").html(point);
-            gift();
-        	});
-            $("#yellow").on("click", function(){
-            point = point + yellow;
-            $("#point").html(point);
-            gift();
-        	});
-        
-        function winner(){
-            alert("You win");
-            win++;
-            $("#wins").html(win);
-            reset();
-        };
-        function losser(){
-            alert("you loss");
-            loss++;
-            $("#losses").html(loss);
-            reset();
-        };
+    function gift(){
+      if (point == ranNum) {
+            winner();
+            } else if (point > ranNum){
+            losser();
+            }
+     };
+
+    // onClick for crystal
+    $("#red").on("click", function(){
+        point = point + red;
+        $("#point").html(point);
+        gift();
+      });
+    $("#blue").on("click", function(){
+        point = point + blue;
+        $("#point").html(point);
+        gift();
+      });
+        $("#green").on("click", function(){
+        point = point + green;
+        $("#point").html(point);
+        gift();
+      });
+        $("#yellow").on("click", function(){
+        point = point + yellow;
+        $("#point").html(point);
+        gift();
+      });
+
+    function winner(){
+        alert("You win");
+        win++;
+        $("#wins").html(win);
+        reset();
+    };
+    function losser(){
+        alert("you loss");
+        loss++;
+        $("#losses").html(loss);
+        reset();
+    };
 });//begin function
